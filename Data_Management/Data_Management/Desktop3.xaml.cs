6d3bdbe3b7d1bfe103df693cbd9ebc6ab4f03018
Dede Wahyu A <101755951+DedeWahyu@users.noreply.github.com>
2022-11-25 18:47:47 +0700

    Add files via upload
    FILE XMAL-WPF STUDENTS DATA MANAGEMENT

---
+++ Data_Management/Data_Management/Desktop3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Data_Management
+{
+    /// <summary>
+    /// Interaction logic for Desktop3.xaml
+    /// </summary>
+    public partial class Desktop3 : Page
+    {
+        public Desktop3()
+        {
+            InitializeComponent();
+        }
+    }
+}
